0dd0a4818762ed41f8856b365f0928fb9cd2bcc4
Shravani27M <1nc25mc022@ncetmail.com>
2026-04-16 14:38:22 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,28 +1,26 @@
 import streamlit as st
-from diffusers import StableDiffusionPipeline
 import torch
+from diffusers import AutoPipelineForText2Image
 
 st.title("🎨 AI Text to Image Generator")
 st.write("Enter a prompt and generate an image!")
 
-# Input
 prompt = st.text_input("Enter your prompt:")
 
-# Button
 if st.button("Generate Image"):
     if not prompt.strip():
         st.warning("Please enter a prompt!")
     else:
         try:
-            with st.spinner("Loading model... ⏳ (first time may take time)"):
-                pipe = StableDiffusionPipeline.from_pretrained(
-                    "runwayml/stable-diffusion-v1-5",
+            with st.spinner("Loading model... ⏳"):
+                pipe = AutoPipelineForText2Image.from_pretrained(
+                    "stabilityai/sdxl-turbo",   # latest fast model
                     torch_dtype=torch.float16
                 )
                 pipe = pipe.to("cuda" if torch.cuda.is_available() else "cpu")
 
             with st.spinner("Generating image... 🎨"):
-                image = pipe(prompt).images[0]
+                image = pipe(prompt, num_inference_steps=1).images[0]
 
             st.success("Here is your generated image:")
             st.image(image, caption=prompt)
